9414c5b26e5e4ce015f8dd0a15025110a339e804
ahmedali0159 <pirates0159@gmail.com>
2021-06-09 20:42:05 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import Dropdown from './components/Home/Dropdown';
+import Navbar from './components/Home/Navbar';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+     if(window.innerWidth > 768 && isOpen){
+       setIsOpen(false);
+       console.log('resized')
+     }
+    };
+    window.addEventListener('resized', hideMenu)
+
+    return  () => {
+      window.removeEventListener('resized', hideMenu);
+    };
+  },)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+    </>
   );
 }
 
---
+++ src/components/Home/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = ({toggle}) => {
+  return (
+    <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-md font-mono" role="navigation">
+      <Link to="/" className="pl-8">
+        Ready Rides.
+      </Link>
+      <div onClick={toggle} className="px-4 cursor-pointer md:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h8m-8 6h16"
+          />
+        </svg>
+      </div>
+      <div className="pr-8 md:block hidden">
+        <Link to="/" className="p-4">Home</Link>
+        <Link to="/about" className="p-4">About Us</Link>
+        <Link to="/process" className="p-4">Process</Link>
+        <Link to="/contact" className="p-4">Contact Us</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
